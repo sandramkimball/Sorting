657dbf7f1cfa791fdb6a078c878fb30fa7cb70a9
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-02-05 19:40:06 -0800

    finished interative_sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,23 +4,26 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        next_index = [cur_index + 1]
+        while smallest_index < next_index:
+            # find next smallest element (hint, can do in 3 loc)  
+            # swap nums if needed
+            if cur_index > next_index:
+                smallest_index = next_index
+            else:
+                cur_index = next_index
 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
+# implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for i in range(2, len(arr) - 1):
+        curr_index = i
 
+        for j in range(0, n - curr_index - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,26 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the help function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    for i in arrA[i] and arrB[i]:
+        if arrA[i] < arrB[i]:
+            merged_arr.append(arrA[i], arrB[i])
+
+        elif arrA[i] > arrB[i]:
+            merged_arr.append(arrB[i], arrA[i])
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    for i in arr:
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+
+    # for i in range(0, n - curr_index - 1):
 
     return arr
 

c96904e6006918ae86266d4d5dccfe126a9b186f
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-02-18 13:36:30 -0800

    finally found typo that kept breaking recursive alg

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,36 +1,37 @@
 # HELPER to merge 2 sorted arrays:
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    a = 0
-    b = 0
+    i = 0
+    j = 0
 
-    for i in range(0, elements):
-        if a >= arrB[b]:
-            merged_arr = arrB[b]
-        elif b >= arrB[b]:
-            merged_arr[i] = arrA[a]
-
-        elif arrA[a] < arrB[b]:
-            merged_arr[i] = arrB[b]
-
+    for k in range(0, elements):
+        if i >= len(arrA):
+            merged_arr[k] = arrB[j]
+            j += 1
+        elif j >= len(arrB):
+            merged_arr[k] = arrA[i]
+            i +=1
+        elif arrA[i] < arrB[j]:
+            merged_arr[k] = arrA[i]
+            i += 1
         else:
-            merged_arr[i] = arrB[b]
-            b += 1
+            merged_arr[k] = arrB[j]
+            j += 1
 
     return merged_arr
+
 
 # Merge Sort using RECURSION:
 def merge_sort( arr ):
     if len(arr) > 1:
-        middle = len(arr)//2
-        left = arr[:middle]
-        right = arr[middle:]
+        midpoint = int(len(arr)//2)
+        left = arr[:midpoint]
+        right = arr[midpoint:]
 
-        l = merge_sort(left)
-        r = merge_sort(right)
+        left = merge_sort(left)
+        right = merge_sort(right)
 
-        arr = merge(l, r)
-
+        arr = merge(left, right)
     return arr
